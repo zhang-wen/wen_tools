91b3dd8350c105f44aad4dc59222e0351ab7501c
Wen Zhang <wzhang.xt@gmail.com>
2023-09-01 17:17:32 +0800

    Update zh_cut_char.py

--- zh_cut_char.py
+++ zh_cut_char.py
@@ -3,6 +3,30 @@ from zhon.hanzi import punctuation
 import sys
 import re
 
+def zhSents_to_zhChars(chinese_sentence):
+
+    chinese_sentence = chinese_sentence.strip()
+    list_chars = []
+    _tmp = ''
+
+    for c in chinese_sentence:
+        if ( ChineseHelper.isChinese(c) ) or ( c in punctuation ) or ( c in string.punctuation ):
+            if _tmp:
+                print(_tmp.strip())
+                list_chars.append(_tmp.strip())
+                _tmp = ''
+            print(c)
+            list_chars.append(c)
+        else:
+            _tmp += c
+
+    if _tmp:
+        list_chars.append(_tmp.strip())
+
+    return list_chars
+
+
+
 for line in sys.stdin:
     _tmp = ''
     for c in line.strip():
